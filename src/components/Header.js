d20bd3f90f82ea8cb59fdee62fd4259e70125253
mkvasconcelos <mateuskvasconcelos@gmail.com>
2022-12-08 20:04:51 -0300

    BONUS 1: A button to return to home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
 import React from "react";
-import styled from 'styled-components'
+import styled from 'styled-components';
+import { BsArrowLeft } from 'react-icons/bs';
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 
 export default function Header() {
+    const location = useLocation();
+    const navigate = useNavigate();
+    const { idFilme } = useParams();
+    console.log(idFilme);
     return (
         <Container>
+            <div data-test="go-home-btn" onClick={() => navigate('../')}>
+                {location.pathname !== "/" && <BsArrowLeft />}
+            </div>
             CINEFLEX
         </Container>
     )
@@ -17,4 +26,13 @@ const Container = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+
+    div{
+        position: fixed;
+        left: 10px;
+        top: 15px;
+        font-size: 37px;
+        color: black;
+        cursor: pointer;
+    }
 `
